0fa313370ae198165b6a33f74edce3b106aef94a
Hongxia Zhong <hongxia.zhong@gmail.com>
2014-11-26 22:01:25 -0800

    adds pseudocode for RequestVote RPC

--- src/Network/Tangaroa/Internal/Monad.hs
+++ src/Network/Tangaroa/Internal/Monad.hs
@@ -9,12 +9,17 @@ module Network.Tangaroa.Internal.Monad
   , becomeLeader
   , becomeCandidate
   , incrementCommitIndex
+  , applyLogEntries
   , cancelTimer
   , setTimedEvent
   , sendAppendEntries
   , sendAppendEntriesResponse
   , handleAppendEntries
   , handleAppendEntriesResponse
+  , sendRequestVote
+  , sendRequestVoteResponse
+  , handleRequestVote
+  , handleRequestVoteResponse
   ) where
 
 import Control.Lens hiding (Index)
@@ -76,6 +81,12 @@ incrementCommitIndex = undefined -- TODO
 --    break
 --self.commitIndex = N-1
 
+applyLogEntries :: Raft nt mt ht ()
+applyLogEntries = undefined -- TODO
+--while self.commitIndex > self.lastApplied:
+--  self.lastApplied++
+--  apply self.log[self.lastApplied]
+
 sendAppendEntries :: RaftSpec nt et rt mt ht -> nt -> Raft nt mt ht ()
 sendAppendEntries = undefined -- TODO
 --AppendEntries ae
@@ -90,6 +101,8 @@ sendAppendEntries = undefined -- TODO
 handleAppendEntries :: RaftSpec nt et rt mt ht -> AppendEntries nt et -> Raft nt mt ht ()
 handleAppendEntries = undefined -- TODO
 --AppendEntriesResponse aer
+--if role != Follower and ae._aeTerm > self.term:
+--  self.becomeFollower
 --aer._aerTerm = self.term
 --if ae._aeTerm < self.term:
 --  aer._success = false
@@ -103,10 +116,8 @@ handleAppendEntries = undefined -- TODO
 --for entry in ae._entries:
 --  if self.log[entry.index] == null:
 --    self.log[entry.index] = entry
---commitIndex = min(ae._leaderCommit, ae._entries[-1].index)
---for entry in self.log[self.commitIndex+1:commitIndex]:
---  commit entry
---self.commitIndex = commitIndex
+--self.commitIndex = min(ae._leaderCommit, ae._entries[-1].index)
+--self.applyLogEntries
 
 sendAppendEntriesResponse :: RaftSpec nt et rt mt ht -> nt -> Raft nt mt ht ()
 sendAppendEntriesResponse = undefined -- TODO
@@ -115,7 +126,7 @@ handleAppendEntriesResponse :: RaftSpec nt et rt mt ht -> AppendEntriesResponse 
 handleAppendEntriesResponse = undefined -- TODO
 --AppendEntries ae
 --AppendEntriesResponse aer
---if aer._aerTerm > self.term:
+--if role != Follower and aer._aerTerm > self.term:
 --  self.term = aer._aerTerm
 --  self.becomeFollower
 --else:
@@ -123,6 +134,43 @@ handleAppendEntriesResponse = undefined -- TODO
 --    self.nextIndex[nt] = ae._prevLogIndex+1+len(ae._entries)
 --    self.matchIndex[nt] = self.nextIndex[nt]
 --    self.incrementCommitIndex
+--    self.applyLogEntries
 --  else:
 --    self.nextIndex[nt] -= 1
 --    retry AppendEntries
+
+sendRequestVote :: RaftSpec nt et rt mt ht -> nt -> Raft nt mt ht ()
+sendRequestVote = undefined -- TODO
+--RequestVote rv
+--rv._rvTerm = self.term
+--rv._candidateId = self.nodeId
+--rv._lastLogIndex = self.log[-1].index
+--rv._lastLogTerm = self.log[-1].term
+
+handleRequestVote :: RaftSpec nt et rt mt ht -> RequestVote nt et -> Raft net met ht ()
+handleRequestVote = undefined -- TODO
+--RequestVoteResponse rvr
+--if role != Follower and rv._rvTerm > self.term:
+--  self.becomeFollower
+--rvr._rvrTerm = self.term
+--if rv._rvTerm < self.term:
+--  rvr._voteGranted = false
+--if (self.votedFor == null or self.votedFor == rv._candidateId)
+--  and (rv._lastLogIndex >= self.log[-1].index and rv._lastLogTerm >= self.log[-1].term)
+--  rvr._voteGranted = true
+--else:
+--  rvr._voteGranted = false
+
+sendRequestVoteResponse :: RaftSpec nt et rt mt ht -> nt -> Raft nt mt ht ()
+sendRequestVoteResponse = undefined -- TODO
+
+handleRequestVoteResponse :: RaftSpec nt et rt mt ht -> RequestVoteResponse -> Raft nt mt ht ()
+handleRequestVoteResponse = undefined -- TODO
+--if role != Follower and rvr._rvrTerm > self.term:
+--  self.term = rvr._aerTerm
+--  self.becomeFollower
+--else:
+--  if rvr._voteGranted:
+--    self.votesAcquired++
+--  if self.votesAcquired >= quorum_size:
+--    self.becomeLeader
