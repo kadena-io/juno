071f4c656a82a76f7acf6fcfc313db9a0a729cb6
Chris Copeland <chrisnc@cs.stanford.edu>
2014-12-08 00:10:19 -0800

    fixed bft*.sh scripts to use the bft versions of the executables

--- bftclient.sh
+++ bftclient.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dist/build/simpleclient/simpleclient -s 10010 10000 10001 10002 10003 10004
+dist/build/bftclient/bftclient -p public_keys.txt -k private_keys/10008.txt -s 10008 10000 10001 10002 10003 10004
